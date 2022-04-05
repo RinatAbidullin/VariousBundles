8b3f8df1bd68034eeaf84d66f07542fab1541789
Rinat Abidullin <rinatabidullin@gmail.com>
2022-04-05 12:53:59 +0500

    Получение с внешнего репозитория секретного ключа и вывод его на экран

--- VariousBundles/ViewController.swift
+++ VariousBundles/ViewController.swift
@@ -8,8 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let keyLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.text = "KEY = \(ANIMAL_SECRET_KEY)"
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        view.addSubview(keyLabel)
+        
+        NSLayoutConstraint.activate([
+            keyLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            keyLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
